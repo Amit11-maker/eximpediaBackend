f4f51ab323d27352bdae1aa5793ad321509333ed
Divaker Soni <divaker@mandelbulbtech.com>
2021-12-15 16:47:01 +0530

    changed for the query day

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -96,9 +96,9 @@ const fetchExploreShipmentsRecords = async (req, res) => {
 
   let payload = req.body;
   //payload.isEngine = true;
-  let maxQueryPerDay = req.plan.max_query_per_day ? payload.resultType.trim() : 10000;
+  let maxQueryPerDay = req.plan.max_query_per_day ? req.plan.max_query_per_day : 10000;
   var output = await TradeModel.findQueryCount(payload.userId, maxQueryPerDay)
-  if (output){
+  if (!output){
     return res.status(200).json({
       message: 'out of search for the day',
     });
