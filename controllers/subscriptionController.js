fe95faa3b4e9ff898320d1a056a26435999194e7
HimansPY <himanshu.panseriya@mandelbulbtech.com>
2022-02-14 19:30:37 +0530

    Change in provider panel

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,17 +1,17 @@
-const TAG = 'subscriptionController';
+const TAG = "subscriptionController";
 
-const EnvConfig = require('../config/envConfig');
+const EnvConfig = require("../config/envConfig");
 
-const ObjectID = require('mongodb').ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 
-const SubscriptionModel = require('../models/subscriptionModel');
-const OrderModel = require('../models/orderModel');
-const AccountModel = require('../models/accountModel');
-const SubscriptionSchema = require('../schemas/subscriptionSchema');
-const OrderSchema = require('../schemas/orderSchema');
-const PaymentSchema = require('../schemas/paymentSchema');
+const SubscriptionModel = require("../models/subscriptionModel");
+const OrderModel = require("../models/orderModel");
+const AccountModel = require("../models/accountModel");
+const SubscriptionSchema = require("../schemas/subscriptionSchema");
+const OrderSchema = require("../schemas/orderSchema");
+const PaymentSchema = require("../schemas/paymentSchema");
 
-const EmailHelper = require('../helpers/emailHelper');
+const EmailHelper = require("../helpers/emailHelper");
 
 const create = (req, res) => {
   let payload = req.body;
@@ -24,7 +24,10 @@ const create = (req, res) => {
   let userId = payload.user_id;
 
   let subscriptionItem = {};
-  if (payload.plan.subscriptionType == SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM) {
+  if (
+    payload.plan.subscriptionType ==
+    SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM
+  ) {
     subscriptionItem = payload.plan;
   }
   subscriptionItem.category = SubscriptionSchema.ITEM_CATEGORY_SUBCRIPTION;
@@ -36,13 +39,16 @@ const create = (req, res) => {
     user_id: userId,
     items: [subscriptionItem],
     offers: [],
-    charges: []
+    charges: [],
   };
   subscriptionOrderPayload.applySubscription = true; // Registration Plan | Custom Plan -> Auto-Activation-Flag
   let order = OrderSchema.buildOrder(subscriptionOrderPayload);
   order.status = OrderSchema.PROCESS_STATUS_SUCCESS;
 
-  if (payload.plan.subscriptionType == SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM) {
+  if (
+    payload.plan.subscriptionType ==
+    SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM
+  ) {
     let paymentPayload = {
       provider: "EXIMPEDIA",
       transaction_id: payload.plan.payment.transaction_id,
@@ -51,87 +57,91 @@ const create = (req, res) => {
       error: null,
       info: {
         mode: PaymentSchema.PAYMENT_MODE_ONLINE_INDIRECT,
-        note: payload.plan.payment.note
+        note: payload.plan.payment.note,
       },
       currency: payload.plan.payment.currency,
-      amount: payload.plan.payment.amount
+      amount: payload.plan.payment.amount,
     };
     let payment = PaymentSchema.buildPayment(paymentPayload);
     order.payments.push(payment);
   }
 
-  let orderItemSubcsription = order.items.filter(item => item.category === SubscriptionSchema.ITEM_CATEGORY_SUBCRIPTION)[0];
+  let orderItemSubcsription = order.items.filter(
+    (item) => item.category === SubscriptionSchema.ITEM_CATEGORY_SUBCRIPTION
+  )[0];
   let accountPlanConstraint = {
-    plan_constraints: orderItemSubcsription.meta
+    plan_constraints: orderItemSubcsription.meta,
   };
-  accountPlanConstraint.plan_constraints.order_item_subscription_id = orderItemSubcsription._id;
+  accountPlanConstraint.plan_constraints.order_item_subscription_id =
+    orderItemSubcsription._id;
 
   OrderModel.add(order, (error, orderEntry) => {
     if (error) {
       res.status(500).json({
-        message: 'Internal Server Error',
+        message: "Internal Server Error",
       });
     } else {
-
-      AccountModel.update(accountId, accountPlanConstraint, (error, accountUpdateStatus) => {
-        if (error) {
-          res.status(500).json({
-            message: 'Internal Server Error',
-          });
-        } else {
-
-          if (accountUpdateStatus) {
-
-            let templateData = {
-              accountAccessUrl: EnvConfig.HOST_WEB_PANEL + 'consumers/accounts/profile',
-              recipientEmail: accountEmailId
-            };
-            let emailTemplate = EmailHelper.buildEmailAccountSubscriptionTemplate(templateData);
-
-            let emailData = {
-              recipientEmail: accountEmailId,
-              subject: 'Account Subscription Activation',
-              html: emailTemplate
-            };
-
-            res.status(200).json({
-              data: {
-                subscription_email_id: accountEmailId
-              }
-            });
-            EmailHelper.triggerEmail(emailData, function (error, mailtriggered) {
-              if (error) {
-                res.status(500).json({
-                  message: 'Internal Server Error',
-                });
-              } else {
-                if (mailtriggered) {
-                  res.status(200).json({
-                    data: {
-                      subscription_email_id: accountEmailId
-                    }
-                  });
-                } else {
-                  res.status(200).json({
-                    data: {}
-                  });
-                }
-              }
-            });
-
-          } else {
+      AccountModel.update(
+        accountId,
+        accountPlanConstraint,
+        (error, accountUpdateStatus) => {
+          if (error) {
             res.status(500).json({
-              message: 'Internal Server Error',
+              message: "Internal Server Error",
             });
+          } else {
+            if (accountUpdateStatus) {
+              let templateData = {
+                accountAccessUrl:
+                  EnvConfig.HOST_WEB_PANEL + "consumers/accounts/profile",
+                recipientEmail: accountEmailId,
+              };
+              let emailTemplate =
+                EmailHelper.buildEmailAccountSubscriptionTemplate(templateData);
+
+              let emailData = {
+                recipientEmail: accountEmailId,
+                subject: "Account Subscription Activation",
+                html: emailTemplate,
+              };
+
+              res.status(200).json({
+                data: {
+                  subscription_email_id: accountEmailId,
+                },
+              });
+              EmailHelper.triggerEmail(
+                emailData,
+                function (error, mailtriggered) {
+                  if (error) {
+                    res.status(500).json({
+                      message: "Internal Server Error",
+                    });
+                  } else {
+                    if (mailtriggered) {
+                      res.status(200).json({
+                        data: {
+                          subscription_email_id: accountEmailId,
+                        },
+                      });
+                    } else {
+                      res.status(200).json({
+                        data: {},
+                      });
+                    }
+                  }
+                }
+              );
+            } else {
+              res.status(500).json({
+                message: "Internal Server Error",
+              });
+            }
           }
-
         }
-      });
-
+      );
     }
-
   });
-
 };
 
 const updateConstraints = (req, res) => {
@@ -146,151 +156,175 @@ const updateConstraints = (req, res) => {
   //let userId = payload.user_id;
 
   let subscriptionItem = {};
-  if (payload.plan.subscriptionType == SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM) {
+  if (
+    payload.plan.subscriptionType ==
+    SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM
+  ) {
     subscriptionItem = payload.plan;
   }
   subscriptionItem.subscriptionType = payload.plan.subscriptionType;
 
-  let constraints = SubscriptionSchema.buildSubscriptionConstraint(subscriptionItem);
+  let constraints =
+    SubscriptionSchema.buildSubscriptionConstraint(subscriptionItem);
 
   let accountPlanConstraint = {
-    plan_constraints: constraints
+    plan_constraints: constraints,
   };
 
-  OrderModel.updateItemSubscriptionConstraints(accountId, subscriptionId, accountPlanConstraint.plan_constraints, (error, orderUpdateStatus) => {
-    if (error) {
-      res.status(500).json({
-        message: 'Internal Server Error',
-      });
-    } else {
-
-      if (orderUpdateStatus) {
-        accountPlanConstraint.plan_constraints.order_item_subscription_id = ObjectID(subscriptionId);
-        AccountModel.update(accountId, accountPlanConstraint, (error, accountUpdateStatus) => {
-          if (error) {
-            res.status(500).json({
-              message: 'Internal Server Error',
-            });
-          } else {
-
-            if (accountUpdateStatus) {
-
-              let templateData = {
-                accountAccessUrl: EnvConfig.HOST_WEB_PANEL + 'consumers/accounts/profile',
-                recipientEmail: accountEmailId
-              };
-              let emailTemplate = EmailHelper.buildEmailAccountSubscriptionTemplate(templateData);
-
-              let emailData = {
-                recipientEmail: accountEmailId,
-                subject: 'Account Subscription Changes Applied',
-                html: emailTemplate
-              };
-
-              res.status(200).json({
-                data: {
-                  subscription_email_id: accountEmailId
-                }
-              });
-              EmailHelper.triggerEmail(emailData, function (error, mailtriggered) {
-                if (error) {
-                  res.status(500).json({
-                    message: 'Internal Server Error',
-                  });
-                } else {
-                  if (mailtriggered) {
-                    res.status(200).json({
-                      data: {
-                        subscription_email_id: accountEmailId
-                      }
-                    });
-                  } else {
-                    res.status(200).json({
-                      data: {}
-                    });
-                  }
-                }
-              });
-
-            } else {
-              res.status(500).json({
-                message: 'Internal Server Error',
-              });
-            }
-
-          }
-        });
-
-      } else {
-
+  OrderModel.updateItemSubscriptionConstraints(
+    accountId,
+    subscriptionId,
+    accountPlanConstraint.plan_constraints,
+    (error, orderUpdateStatus) => {
+      if (error) {
         res.status(500).json({
-          message: 'Internal Server Error',
+          message: "Internal Server Error",
         });
+      } else {
+        if (orderUpdateStatus) {
+          accountPlanConstraint.plan_constraints.order_item_subscription_id =
+            ObjectID(subscriptionId);
+          AccountModel.update(
+            accountId,
+            accountPlanConstraint,
+            (error, accountUpdateStatus) => {
+              if (error) {
+                res.status(500).json({
+                  message: "Internal Server Error",
+                });
+              } else {
+                if (accountUpdateStatus) {
+                  let templateData = {
+                    accountAccessUrl:
+                      EnvConfig.HOST_WEB_PANEL + "consumers/accounts/profile",
+                    recipientEmail: accountEmailId,
+                  };
+                  let emailTemplate =
+                    EmailHelper.buildEmailAccountSubscriptionTemplate(
+                      templateData
+                    );
 
+                  let emailData = {
+                    recipientEmail: accountEmailId,
+                    subject: "Account Subscription Changes Applied",
+                    html: emailTemplate,
+                  };
+
+                  res.status(200).json({
+                    data: {
+                      subscription_email_id: accountEmailId,
+                    },
+                  });
+                  EmailHelper.triggerEmail(
+                    emailData,
+                    function (error, mailtriggered) {
+                      if (error) {
+                        res.status(500).json({
+                          message: "Internal Server Error",
+                        });
+                      } else {
+                        if (mailtriggered) {
+                          res.status(200).json({
+                            data: {
+                              subscription_email_id: accountEmailId,
+                            },
+                          });
+                        } else {
+                          res.status(200).json({
+                            data: {},
+                          });
+                        }
+                      }
+                    }
+                  );
+                } else {
+                  res.status(500).json({
+                    message: "Internal Server Error",
+                  });
+                }
+              }
+            }
+          );
+        } else {
+          res.status(500).json({
+            message: "Internal Server Error",
+          });
+        }
       }
-
     }
-
-  });
-
+  );
 };
 
 const fetchSubscriptions = (req, res) => {
-
   let payloadParams = req.params;
   let payload = req.query;
 
-  const tradeTotalRecords = (payload.tradeTotalRecords) ? payload.tradeTotalRecords : null;
-  const pageKey = (payload.draw && payload.draw != 0) ? payload.draw : null;
+  const tradeTotalRecords = payload.tradeTotalRecords
+    ? payload.tradeTotalRecords
+    : null;
+  const pageKey = payload.draw && payload.draw != 0 ? payload.draw : null;
   let offset = null;
   let limit = null;
   //Datatable JS Mode
   if (pageKey != null) {
-    offset = (payload.start != null) ? payload.start : 0;
-    limit = (payload.length != null) ? payload.length : 10;
+    offset = payload.start != null ? payload.start : 0;
+    limit = payload.length != null ? payload.length : 10;
   } else {
-    offset = (payload.offset != null) ? payload.offset : 0;
-    limit = (payload.limit != null) ? payload.limit : 10;
+    offset = payload.offset != null ? payload.offset : 0;
+    limit = payload.limit != null ? payload.limit : 10;
   }
 
-  SubscriptionModel.findByAccount(payloadParams.accountId, null, offset, limit, (error, subscriptionDataPack) => {
-    if (error) {
-      res.status(500).json({
-        message: 'Internal Server Error',
-      });
-    } else {
-      let bundle = {};
-      if (!subscriptionDataPack) {
-        bundle.recordsTotal = 0;
-        bundle.recordsFiltered = 0;
-        bundle.error = "Unrecognised Shipments Response"; //Show if to be interpreted as error on client-side
+  SubscriptionModel.findByAccount(
+    payloadParams.accountId,
+    null,
+    offset,
+    limit,
+    (error, subscriptionDataPack) => {
+      if (error) {
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
       } else {
-        let recordsTotal = (subscriptionDataPack[SubscriptionSchema.RESULT_PORTION_TYPE_SUMMARY].length > 0) ? subscriptionDataPack[SubscriptionSchema.RESULT_PORTION_TYPE_SUMMARY][0].count : 0;
-        bundle.recordsTotal = (tradeTotalRecords != null) ? tradeTotalRecords : recordsTotal;
-        bundle.recordsFiltered = recordsTotal;
+        let bundle = {};
+        if (!subscriptionDataPack) {
+          bundle.recordsTotal = 0;
+          bundle.recordsFiltered = 0;
+          bundle.error = "Unrecognised Shipments Response"; //Show if to be interpreted as error on client-side
+        } else {
+          let recordsTotal =
+            subscriptionDataPack[SubscriptionSchema.RESULT_PORTION_TYPE_SUMMARY]
+              .length > 0
+              ? subscriptionDataPack[
+                  SubscriptionSchema.RESULT_PORTION_TYPE_SUMMARY
+                ][0].count
+              : 0;
+          bundle.recordsTotal =
+            tradeTotalRecords != null ? tradeTotalRecords : recordsTotal;
+          bundle.recordsFiltered = recordsTotal;
+        }
+
+        if (pageKey) {
+          bundle.draw = pageKey;
+        }
+
+        bundle.data =
+          subscriptionDataPack[SubscriptionSchema.RESULT_PORTION_TYPE_RECORDS];
+
+        res.status(200).json(bundle);
       }
-
-      if (pageKey) {
-        bundle.draw = pageKey;
-      }
-
-      bundle.data = subscriptionDataPack[SubscriptionSchema.RESULT_PORTION_TYPE_RECORDS];
-
-      res.status(200).json(bundle);
     }
-  });
+  );
 };
 
 const fetchSubscriptionPlanTemplates = (req, res) => {
   res.status(200).json({
-    data: SubscriptionSchema.getSubscriptionPlans()
+    data: SubscriptionSchema.getSubscriptionPlans(),
   });
 };
-
 
 module.exports = {
   create,
   updateConstraints,
   fetchSubscriptions,
-  fetchSubscriptionPlanTemplates
+  fetchSubscriptionPlanTemplates,
 };
--- schemas/orderSchema.js
+++ schemas/orderSchema.js
@@ -1,31 +1,29 @@
-const TAG = 'orderSchema';
+const TAG = "orderSchema";
 
-const SubscriptionSchema = require('./subscriptionSchema');
-const PaymentSchema = require('./paymentSchema');
-const {
-  SUBSCRIPTION_PLAN_TYPE_CUSTOM
-} = require('./subscriptionSchema');
+const SubscriptionSchema = require("./subscriptionSchema");
+const PaymentSchema = require("./paymentSchema");
+const { SUBSCRIPTION_PLAN_TYPE_CUSTOM } = require("./subscriptionSchema");
 
-const ObjectID = require('mongodb').ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 
-const PROCESS_STATUS_INITIATING = 'INITIATING';
-const PROCESS_STATUS_INITIATED = 'INITIATED';
-const PROCESS_STATUS_PROCESSING = 'PROCESSING';
-const PROCESS_STATUS_SUCCESS = 'SUCCESS';
-const PROCESS_STATUS_FAILED = 'FAILED';
-const PROCESS_STATUS_CANCELLED = 'CANCELLED';
-const PROCESS_STATUS_RETURNED = 'RETURNED';
-const PROCESS_STATUS_UNKNOWN = 'UNKNOWN';
+const PROCESS_STATUS_INITIATING = "INITIATING";
+const PROCESS_STATUS_INITIATED = "INITIATED";
+const PROCESS_STATUS_PROCESSING = "PROCESSING";
+const PROCESS_STATUS_SUCCESS = "SUCCESS";
+const PROCESS_STATUS_FAILED = "FAILED";
+const PROCESS_STATUS_CANCELLED = "CANCELLED";
+const PROCESS_STATUS_RETURNED = "RETURNED";
+const PROCESS_STATUS_UNKNOWN = "UNKNOWN";
 
-const SEPARATOR_UNDERSCORE = '_';
-const SEPARATOR_SPACE = ' ';
+const SEPARATOR_UNDERSCORE = "_";
+const SEPARATOR_SPACE = " ";
 
 const order = {
-  account_id: '',
-  user_id: '',
-  receipt_uid: '',
+  account_id: "",
+  user_id: "",
+  receipt_uid: "",
   status: PROCESS_STATUS_INITIATING,
-  currency: '',
+  currency: "",
   amount: 0,
   items: [],
   offer: [],
@@ -33,23 +31,23 @@ const order = {
   billing: {},
   payments: [],
   created_ts: 0,
-  modified_ts: 0
+  modified_ts: 0,
 };
 
 const billing = {
   name: {
-    first_name: '',
-    last_name: ''
+    first_name: "",
+    last_name: "",
   },
-  email_id: '',
-  mobile_no: '',
+  email_id: "",
+  mobile_no: "",
   address: {
-    place: '',
-    postal_code: '',
-    city: '',
-    state: '',
-    country: ''
-  }
+    place: "",
+    postal_code: "",
+    city: "",
+    state: "",
+    country: "",
+  },
 };
 
 const buildOrder = (data) => {
@@ -59,16 +57,20 @@ const buildOrder = (data) => {
   content.user_id = ObjectID(data.user_id);
   content.receipt_uid = `EXIM-${currentTimestamp}`;
   let orderAmount = 0;
-  data.items.forEach(item => {
+  data.items.forEach((item) => {
     if (item.category === SubscriptionSchema.ITEM_CATEGORY_SUBCRIPTION) {
+      let selectedPlan = SubscriptionSchema.subscriptionsPlans.filter(
+        (plan) => plan.type === item.subscriptionType
+      )[0];
 
-      let selectedPlan = SubscriptionSchema.subscriptionsPlans.filter(plan => plan.type === item.subscriptionType)[0];
-
-      if (selectedPlan.type === SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM) {
+      if (
+        selectedPlan.type === SubscriptionSchema.SUBSCRIPTION_PLAN_TYPE_CUSTOM
+      ) {
         item.price = {};
         item.price.currency = item.payment.currency;
         item.price.amount = item.payment.amount;
-        selectedPlan = SubscriptionSchema.deriveCustomSubscriptionPlanDetail(item);
+        selectedPlan =
+          SubscriptionSchema.deriveCustomSubscriptionPlanDetail(item);
       }
 
       let itemBundle = {
@@ -76,8 +78,8 @@ const buildOrder = (data) => {
         category: SubscriptionSchema.ITEM_CATEGORY_SUBCRIPTION,
         detail: JSON.parse(JSON.stringify(selectedPlan)),
         meta: {
-          is_active: 0
-        }
+          is_active: 0,
+        },
       };
       if (data.applySubscription != null && data.applySubscription) {
         itemBundle.meta = SubscriptionSchema.buildSubscriptionConstraint(item);
@@ -85,6 +87,8 @@ const buildOrder = (data) => {
         itemBundle.meta.subscribed_ts = currentTimestamp;
         itemBundle.meta.is_hidden = item.is_hidden;
         itemBundle.meta.max_query_per_day = item.max_query_per_day;
+        itemBundle.meta.max_save_query = Number(item.max_save_query);
+        itemBundle.meta.max_workspace_count = Number(item.max_workspace_count);
         //itemBundle.created_ts = currentTimestamp;
         //itemBundle.modified_ts = currentTimestamp;
       }
@@ -93,15 +97,16 @@ const buildOrder = (data) => {
       orderAmount += selectedPlan.price.amount;
       content.currency = selectedPlan.price.currency;
     } else if (item.category === SubscriptionSchema.ITEM_CATEGORY_TOP_UP) {
-
-      let selectedTopUp = SubscriptionSchema.topUpPlans.filter(plan => plan.type === item.topUpType)[0];
+      let selectedTopUp = SubscriptionSchema.topUpPlans.filter(
+        (plan) => plan.type === item.topUpType
+      )[0];
       let itemBundle = {
         _id: new ObjectID(),
         category: SubscriptionSchema.ITEM_CATEGORY_TOP_UP,
         detail: JSON.parse(JSON.stringify(selectedTopUp)),
         meta: {
-          is_active: 0
-        }
+          is_active: 0,
+        },
       };
       if (data.applySubscription != null && data.applySubscription) {
         itemBundle.meta = SubscriptionSchema.buildTopUpConstraint(item);
@@ -114,13 +119,12 @@ const buildOrder = (data) => {
 
       orderAmount += selectedTopUp.price.amount;
       content.currency = selectedTopUp.price.currency;
-
     }
   });
-  data.offers.forEach(offer => {
+  data.offers.forEach((offer) => {
     orderAmount -= offer.price.amount;
   });
-  data.charges.forEach(offer => {
+  data.charges.forEach((offer) => {
     orderAmount += offer.price.amount;
   });
   content.amount = orderAmount;
@@ -137,5 +141,5 @@ module.exports = {
   PROCESS_STATUS_FAILED,
   PROCESS_STATUS_CANCELLED,
   PROCESS_STATUS_UNKNOWN,
-  buildOrder
+  buildOrder,
 };
