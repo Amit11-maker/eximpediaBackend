67774b4cf6429b501636cefda7cc136ffce6f725
Divaker Soni <divaker@mandelbulbtech.com>
2022-02-21 13:26:34 +0530

    change in cors

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ const ElasticSearchDbHandler = require("./db/elasticsearchDbHandler");
 
 const corsOptions = {
   origin: (origin, callback) => {
+    if (origin == undefined){
+      callback(null, true);
+      return
+    }
     if (process.env.HOST.split("|").some(e => origin.includes(e)) || !origin) {
       callback(null, true);
       return
