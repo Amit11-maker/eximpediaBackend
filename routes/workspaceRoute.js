2e6d7ca5e7875597d40288ff227bcb483a7b12bb
Divaker Soni <divaker@mandelbulbtech.com>
2022-05-25 10:49:04 +0530

    removed authentication from workspace API

--- routes/workspaceRoute.js
+++ routes/workspaceRoute.js
@@ -10,33 +10,33 @@ const WorkspaceController = require('../controllers/workspaceController');
 const AuthMiddleware = require('../middlewares/authMiddleware');
 
 // Log Time
-router.use(function timeLog(req, res, next) {
+router.use(function timeLog (req, res, next) {
   //console.log('Time: ', Date.now());
   next();
 });
 
 //Command Segregation
 
-router.post('/', AuthMiddleware.authorizeAccess ,WorkspaceController.create);
-router.delete('/:workspaceId',AuthMiddleware.authorizeAccess, WorkspaceController.remove);
-router.post('/records/purchase/approval', AuthMiddleware.authorizeAccess, WorkspaceController.approveRecordsPurchaseEngine); // Aliased GET //approveRecordsPurchaseEngine
-router.post('/records', AuthMiddleware.authorizeAccess, WorkspaceController.addRecordsEngine); //addRecordsEngine addRecords
-router.put('/:workspaceId', AuthMiddleware.authorizeAccess ,WorkspaceController.updateRecordMetrics);
+router.post('/', WorkspaceController.create);
+router.delete('/:workspaceId', WorkspaceController.remove);
+router.post('/records/purchase/approval', WorkspaceController.approveRecordsPurchaseEngine); // Aliased GET //approveRecordsPurchaseEngine
+router.post('/records', WorkspaceController.addRecordsEngine); //addRecordsEngine addRecords
+router.put('/:workspaceId', WorkspaceController.updateRecordMetrics);
 
-router.post('/shipments/analytics/records',AuthMiddleware.authorizeAccess, WorkspaceController.fetchAnalyticsShipmentsRecords); // Aliased GET
-router.post('/shipments/analytics/statistics',AuthMiddleware.authorizeAccess, WorkspaceController.fetchAnalyticsShipmentsStatistics); // Aliased GET
+router.post('/shipments/analytics/records', WorkspaceController.fetchAnalyticsShipmentsRecords); // Aliased GET
+router.post('/shipments/analytics/statistics', WorkspaceController.fetchAnalyticsShipmentsStatistics); // Aliased GET
 
-router.post('/shipments/analytics/records/file', AuthMiddleware.authorizeAccess, WorkspaceController.fetchAnalyticsShipmentRecordsFile);
+router.post('/shipments/analytics/records/file', WorkspaceController.fetchAnalyticsShipmentRecordsFile);
 // router.get('/shipments/records/file', WorkspaceController.fetchShipmentRecordsFile);
 
 // Query Segregation
-router.get('/', AuthMiddleware.authorizeAccess ,WorkspaceController.fetchByUser);
+router.get, WorkspaceController.fetchByUser);
 router.post('/share', AuthMiddleware.authorizeAccess, WorkspaceController.shareWorkspace);
 router.get('/list/:userId', AuthMiddleware.authorizeAccess, WorkspaceController.listWorkspace);
 router.get('/templates', AuthMiddleware.authorizeAccess, WorkspaceController.fetchWorkspaceTemplates);
-router.get('/existence/verification', AuthMiddleware.authorizeAccess, WorkspaceController.verifyWorkspaceExistence);
+router.get('/existence/verification', WorkspaceController.verifyWorkspaceExistence);
 
-router.get('/:workspaceId/analytics/specifications', AuthMiddleware.authorizeAccess ,WorkspaceController.fetchAnalyticsSpecification);
-router.post('/shipments/analytics/traders/search',AuthMiddleware.authorizeAccess, WorkspaceController.fetchAnalyticsShipmentsTradersByPatternEngine); //fetchAnalyticsShipmentsTradersByPattern
+router.get('/:workspaceId/analytics/specifications', WorkspaceController.fetchAnalyticsSpecification);
+router.post('/shipments/analytics/traders/search', WorkspaceController.fetchAnalyticsShipmentsTradersByPatternEngine); //fetchAnalyticsShipmentsTradersByPattern
 
 module.exports = router;
