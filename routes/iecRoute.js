fcc7fb473167d06cba526fe461cd8ef3e4baf487
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2022-06-25 20:03:36 +0530

    IEC details api changes

---
+++ routes/iecRoute.js
@@ -0,0 +1,18 @@
+const TAG = "iecRoute";
+
+const express = require('express');
+const router = express.Router({
+  mergeParams: true
+});
+
+const IECController = require('../controllers/iecController');
+const AuthMiddleware = require('../middlewares/authMiddleware');
+
+// Log Time
+router.use(function timeLog(req, res, next) {
+    //console.log('Time: ', Date.now());
+    next();
+});
+
+router.get('/search/:iecNumber' , AuthMiddleware.authorizeAccess, IECController.fetchIECDetails);
+module.exports = router ;
