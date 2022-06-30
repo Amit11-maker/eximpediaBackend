68e188951178f5fa585ad4f32a09dc107853a868
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2022-06-30 21:52:37 +0530

    Revert "React changes or FetchCustomer changes"
    This reverts commit cb7fde723c326b84e0b2da6b667a76848286e52c.

--- models/saveQueryModel.js
+++ models/saveQueryModel.js
@@ -100,18 +100,16 @@ const findTradeShipmentRecordsAggregation = (
 };
 
 const findSaveQuery = (account_id, cb) => {
-  if(account_id !== undefined){
-    MongoDbHandler.getDbInstance()
-      .collection(MongoDbHandler.collections.saveQuery)
-      .find({ account_id: ObjectID(account_id) })
-      .toArray(function (err, result) {
-        if (err) {
-          cb(err);
-        } else {
-          cb(null, result);
-        }
-      });
-  }
+  MongoDbHandler.getDbInstance()
+    .collection(MongoDbHandler.collections.saveQuery)
+    .find({ account_id: ObjectID(account_id) })
+    .toArray(function (err, result) {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, result);
+      }
+    });
 };
 
 const findTradeShipmentRecordsAggregationEngine = async (
