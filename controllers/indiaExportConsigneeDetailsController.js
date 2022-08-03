41c1aa7e678d9dfc0c2319f4c59738dc84156cea
Gaurav Soni <divaker@mandelbulbtech.com>
2022-08-03 16:58:39 +0530

    cosignee changes

--- controllers/indiaExportConsigneeDetailsController.js
+++ controllers/indiaExportConsigneeDetailsController.js
@@ -3,7 +3,7 @@ const TAG = "IndiaExportConsigneeDetailsController";
 const ConsigneeDetailsModel = require("../models/indiaExportConsigneeDetailsModel");
 
 /** Controller function to add customer requests */
-async function addCustomerRequest(req, res) {
+async function addCustomerRequest (req, res) {
     console.log("Method = addCustomerRequest , Entry");
     const payload = req.body;
     payload.email_id = req.user.email_id;
@@ -11,7 +11,7 @@ async function addCustomerRequest(req, res) {
     if (maxShipmentCount == 0) {
         console.log("Method = addCustomerRequest , Exit");
         res.status(409).json({
-            message : "Request shipment limit reached...Please contact administrator for more shipment requests."
+            message: "Request shipment limit reached...Please contact administrator for more shipment requests."
         });
     }
     else {
@@ -34,11 +34,11 @@ async function addCustomerRequest(req, res) {
 }
 
 /** Controller function to get list of customers requests */
-async function getRequestsList(req, res) {
+async function getRequestsList (req, res) {
     console.log("Method = getRequestsList , Entry");
     try {
         const requestsList = await ConsigneeDetailsModel.getRequestsList();
-        res.status(200).json({requestsList});
+        res.status(200).json(requestsList);
     }
     catch (error) {
         console.log("Method = getRequestsList, Error = ", error)
@@ -52,7 +52,7 @@ async function getRequestsList(req, res) {
 }
 
 /** Controller function to update request response */
-async function updateRequestResponse(req, res) {
+async function updateRequestResponse (req, res) {
     console.log("Method = updateRequestResponse, Entry");
     const payload = req.body;
     try {
@@ -77,33 +77,40 @@ async function updateRequestResponse(req, res) {
 }
 
 /** */
-async function getCosigneeDetailForUser(req, res) {
+async function getCosigneeDetailForUser (req, res) {
     console.log("Method = getCosigneeDetailForUser, Entry");
     const userId = req.user.user_id;
     const shipment_number = req.body.shipment_number;
     try {
         const userRequestData = await ConsigneeDetailsModel.getUserRequestData(userId);
-        const isRequestedShipment = !!userRequestData.requested_shipments.find((shipment) => {
-            return shipment.bill_number === shipment_number;
-        });
-        const isAvailableShipment = !!userRequestData.available_shipments.find((shipment) => {
-            return shipment === shipment_number;
-        });
-        if (isAvailableShipment) {
-            const shipmentData = await ConsigneeDetailsModel.getShipmentData(shipment_number);
-            res.status(200).json({
-                data: shipmentData
-            });
-        }
-        else if (isRequestedShipment) {
-            res.status(200).json({
-                message: "Shipment Data Request is in process ."
-            });
-        }
-        else {
+        if (userRequestData == undefined) {
             res.status(200).json({
                 message: "Request Cosignee Data"
             });
+        }
+        else {
+            const isRequestedShipment = !!userRequestData.requested_shipments.find((shipment) => {
+                return shipment.bill_number === shipment_number;
+            });
+            const isAvailableShipment = !!userRequestData.available_shipments.find((shipment) => {
+                return shipment === shipment_number;
+            });
+            if (isAvailableShipment) {
+                const shipmentData = await ConsigneeDetailsModel.getShipmentData(shipment_number);
+                res.status(200).json({
+                    data: shipmentData
+                });
+            }
+            else if (isRequestedShipment) {
+                res.status(200).json({
+                    message: "Shipment Data Request is in process ."
+                });
+            }
+            else {
+                res.status(200).json({
+                    message: "Request Cosignee Data"
+                });
+            }
         }
     }
     catch (error) {
