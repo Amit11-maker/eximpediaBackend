057ac12e7955d71b9844e0d4511f2446ef79ab5d
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2022-08-12 14:45:46 +0530

    fav_shipment limit bug fix

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -63,7 +63,7 @@ const createShipmentRecommendation = (req, res) => {
   let payload = req.body;
   payload.user_id = req.user.user_id;
   payload.account_id = req.user.account_id;
-  let max_count = req.plan.favorite_shipment_limits;
+  let max_count = req.plan.favorite_shipment_limit;
 
   const count = recommendationSchema.fetchCountSchema(payload);
   recommendationModel.countShipment(count, (error, totalCount) => {
