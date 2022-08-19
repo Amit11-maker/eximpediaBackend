0d0eb5d4cf5ce01beba1cd208fb08d8dbf0bf513
Gaurav Soni <divaker@mandelbulbtech.com>
2022-08-19 13:11:05 +0530

    add error handling in middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,52 +11,63 @@ function authorizeAccess(req, res, next) {
   else {
     bundle = req.cookies;
   }
+  try{
 
-  if (bundle && bundle.token) {
-    TokenHelper.verifyJWTAccessToken(bundle, function (error, payload) {
-      if (error) {
-        return res.status(500).json({
-          message: 'Internal Server Error',
-        });
-      } else {
-        req.user = payload.user;
-        req.plan = payload.plan;
-      
-        if (new Date(payload.plan.access_validity_interval.end_date) < new Date()) {
-          return res.status(401).json({
-            data: {
-              type: 'UNAUTHORISED',
-              msg: 'Plan Expired! Please reach out to provider',
-              desc: 'Invalid Access'
-            }
+    if (bundle && bundle.token) {
+      TokenHelper.verifyJWTAccessToken(bundle, function (error, payload) {
+        if (error) {
+          return res.status(500).json({
+            message: 'Internal Server Error',
           });
+        } else {
+          req.user = payload.user;
+          req.plan = payload.plan;
+        
+          if (new Date(payload.plan.access_validity_interval.end_date) < new Date()) {
+            return res.status(401).json({
+              data: {
+                type: 'UNAUTHORISED',
+                msg: 'Plan Expired! Please reach out to provider',
+                desc: 'Invalid Access'
+              }
+            });
+          }
+          var timeStamp = undefined
+          var flagValue = undefined
+          var fiveFlag = false
+          var tenFlag = false
+          if (((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+            / 86400000) <= 5) {
+            timeStamp = new Date().getTime()
+            fiveFlag = true
+            flagValue = "five"
+          }
+          else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+          / 86400000) <= 10) && !fiveFlag) {
+            timeStamp = new Date().getTime()
+            tenFlag = true
+            flagValue = "ten"
+          }
+          else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+          / 86400000) <= 15) && !tenFlag) {
+            timeStamp = new Date().getTime()
+            flagValue = "fifteen"
+          }
+          NotificationModel.fetchAccountNotification(payload.user.account_id, timeStamp, flagValue)
+          next();
         }
-        var timeStamp = undefined
-        var flagValue = undefined
-        var fiveFlag = false
-        var tenFlag = false
-        if (((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-          / 86400000) <= 5) {
-          timeStamp = new Date().getTime()
-          fiveFlag = true
-          flagValue = "five"
+      });
+    } else {
+      return res.status(401).json({
+        data: {
+          type: 'UNAUTHORISED',
+          msg: 'Access Denied',
+          desc: 'Invalid Access'
         }
-        else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-        / 86400000) <= 10) && !fiveFlag) {
-          timeStamp = new Date().getTime()
-          tenFlag = true
-          flagValue = "ten"
-        }
-        else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-        / 86400000) <= 15) && !tenFlag) {
-          timeStamp = new Date().getTime()
-          flagValue = "fifteen"
-        }
-        NotificationModel.fetchAccountNotification(payload.user.account_id, timeStamp, flagValue)
-        next();
-      }
-    });
-  } else {
+      });
+    }
+  }catch(err){
+    console.log(err)
     return res.status(401).json({
       data: {
         type: 'UNAUTHORISED',
@@ -65,6 +76,7 @@ function authorizeAccess(req, res, next) {
       }
     });
   }
+
 
 }
 
