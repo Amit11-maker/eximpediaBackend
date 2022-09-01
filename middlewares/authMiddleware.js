f16a2f905952c24d59fd2f2f3513528f0569b45e
Gaurav Soni <g.soni@xfusiontech.com>
2022-09-01 16:14:39 +0530

    session timeout bug fixed

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const TokenHelper = require('../helpers/tokenHelper');
 const AccountModel = require("../models/accountModel")
 const NotificationModel = require('../models/notificationModel');
 
-function authorizeAccess(req, res, next) {
+function authorizeAccess (req, res, next) {
   let bundle = {};
   if (req.headers.react && req.headers.react === 'true') {
     bundle.token = req.headers.cookies;
@@ -21,23 +21,57 @@ function authorizeAccess(req, res, next) {
           });
         } else {
           try {
-            if (payload.hasOwnProperty('isFlag')) {
-              let flag = await AccountModel.insertSessionFlag(payload.user_id);
-              // console.log(flag);
-              return res.status(401).json({
-                data: {
-                  type: 'UNAUTHORISED',
-                  msg: 'Plan Expired! Please reach out to provider',
-                  desc: 'Invalid Access'
-                }
-              });
-            } else {
-              if (payload.islogin) {
+            if (payload) {
+              if (payload.hasOwnProperty('isFlag')) {
+                let flag = await AccountModel.insertSessionFlag(payload.user_id);
+                // console.log(flag);
+                return res.status(401).json({
+                  data: {
+                    type: 'UNAUTHORISED',
+                    msg: 'Plan Expired! Please reach out to provider',
+                    desc: 'Invalid Access'
+                  }
+                });
+              } else {
+                if (payload.islogin) {
 
-                req.user = payload.user;
-                req.plan = payload.plan;
+                  req.user = payload.user;
+                  req.plan = payload.plan;
 
-                if (new Date(payload.plan.access_validity_interval.end_date) < new Date()) {
+                  if (new Date(payload.plan.access_validity_interval.end_date) < new Date()) {
+                    return res.status(401).json({
+                      data: {
+                        type: 'UNAUTHORISED',
+                        msg: 'Plan Expired! Please reach out to provider',
+                        desc: 'Invalid Access'
+                      }
+                    });
+                  }
+                  var timeStamp = undefined
+                  var flagValue = undefined
+                  var fiveFlag = false
+                  var tenFlag = false
+                  if (((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+                    / 86400000) <= 5) {
+                    timeStamp = new Date().getTime()
+                    fiveFlag = true
+                    flagValue = "five"
+                  }
+                  else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+                    / 86400000) <= 10) && !fiveFlag) {
+                    timeStamp = new Date().getTime()
+                    tenFlag = true
+                    flagValue = "ten"
+                  }
+                  else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
+                    / 86400000) <= 15) && !tenFlag) {
+                    timeStamp = new Date().getTime()
+                    flagValue = "fifteen"
+                  }
+                  NotificationModel.fetchAccountNotification(payload.user.account_id, timeStamp, flagValue)
+                  next();
+                } else {
+                  let flag = AccountModel.updateSessionFlag(payload.user.user_id);
                   return res.status(401).json({
                     data: {
                       type: 'UNAUTHORISED',
@@ -46,39 +80,15 @@ function authorizeAccess(req, res, next) {
                     }
                   });
                 }
-                var timeStamp = undefined
-                var flagValue = undefined
-                var fiveFlag = false
-                var tenFlag = false
-                if (((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-                  / 86400000) <= 5) {
-                  timeStamp = new Date().getTime()
-                  fiveFlag = true
-                  flagValue = "five"
-                }
-                else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-                  / 86400000) <= 10) && !fiveFlag) {
-                  timeStamp = new Date().getTime()
-                  tenFlag = true
-                  flagValue = "ten"
-                }
-                else if ((((new Date(payload.plan.access_validity_interval.end_date) - new Date())
-                  / 86400000) <= 15) && !tenFlag) {
-                  timeStamp = new Date().getTime()
-                  flagValue = "fifteen"
-                }
-                NotificationModel.fetchAccountNotification(payload.user.account_id, timeStamp, flagValue)
-                next();
-              } else {
-                let flag = AccountModel.updateSessionFlag(payload.user.user_id);
-                return res.status(401).json({
-                  data: {
-                    type: 'UNAUTHORISED',
-                    msg: 'Plan Expired! Please reach out to provider',
-                    desc: 'Invalid Access'
-                  }
-                });
               }
+            } else {
+              return res.status(401).json({
+                data: {
+                  type: 'UNAUTHORISED',
+                  msg: 'Access Denied',
+                  desc: 'Invalid Access'
+                }
+              });
             }
           } catch (err) {
             console.log(err)
