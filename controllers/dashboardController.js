1fc4d108e5be3e25eda0db7e9769b9f0aa9a4c3e
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2022-11-11 14:26:15 +0530

    dashboard fix

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -9,21 +9,93 @@ const fetchConsumersDashboardDetails = async (req, res) => {
   let userId = req.user.user_id ? req.user.user_id.trim() : null;
   let getCountryCount = "";
   if (req.user.role == "ADMINISTRATOR") {
-    const dashboardData = await fetchConsumersDashboardByAccount(accountId, res);
-    res.status(200).json({
-      data: dashboardData,
-      role: req.user.role
-    })
+    try {
+      const dashboardData = await fetchConsumersDashboardByAccount(accountId);
+      res.status(200).json({
+        data: dashboardData,
+        role: req.user.role
+      });
+    } catch (error) {
+      if (error == "Data missing") {
+        res.status(404).json({
+          data: {
+            type: "MISSING",
+            msg: "Details Unavailable",
+            desc: "Details Not Found",
+          },
+        });
+      }
+      else {
+        logger.error(` DASHBOARD CONTROLLER == ${JSON.stringify(error)}`);
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    }
   }
   else {
-    const dashboardData = await fetchConsumersDashboardByUser(accountId, userId, res);
-    res.status(200).json({
-      data: dashboardData,
-      role: req.user.role
-    })
+    try {
+      const dashboardData = await fetchConsumersDashboardByUser(userId);
+      res.status(200).json({
+        data: dashboardData,
+        role: req.user.role
+      });
+    } catch (error) {
+      if (error == "Data missing") {
+        res.status(404).json({
+          data: {
+            type: "MISSING",
+            msg: "Details Unavailable",
+            desc: "Details Not Found",
+          },
+        });
+      }
+      else {
+        logger.error(` DASHBOARD CONTROLLER == ${JSON.stringify(error)}`);
+        res.status(500).json({
+          message: "Internal Server Error",
+        });
+      }
+    }
   }
 }
 
+async function fetchConsumersDashboardByAccount(accountId) {
+  try {
+    const consumerDetails = await DashboardModel.findConsumerByAccount(accountId);
+    if (consumerDetails) {
+      const lower = consumerDetails[0].countryArray.map(element => {
+        return element.toLowerCase();
+      });
+      const uniqueCount = new Set(lower).size;
+      consumerDetails[0].countryArray = uniqueCount;
+     
+      return consumerDetails;
+    } else {
+      throw "Data missing";
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function fetchConsumersDashboardByUser(userId) {
+  try {
+    const consumerDetails = await DashboardModel.findConsumerByUser(userId);
+    if (consumerDetails) {
+      const lower = consumerDetails[0].countryArray.map(element => {
+        return element.toLowerCase();
+      });
+      const uniqueCount = new Set(lower).size;
+      consumerDetails[0].countryArray = uniqueCount;
+      return consumerDetails;
+    } else {
+      throw "Data missing";
+    }
+  } catch (error) {
+    throw error ;
+  }
+}
 
 const fetchProvidersDashboardDetails = (req, res) => {
   DashboardModel.findProviderByAccount((error, customersCount) => {
@@ -88,68 +160,6 @@ const fetchProvidersDashboardDetails = (req, res) => {
       }
     }
   });
-};
-
-async function fetchConsumersDashboardByAccount(accountId, res) {
-  try {
-    const consumerDetails = await DashboardModel.findConsumerByAccount(accountId);
-    if (consumerDetails) {
-      const lower = consumerDetails[0].countryArray.map(element => {
-        return element.toLowerCase();
-      });
-      const uniqueCount = new Set(lower).size;
-      consumerDetails[0].countryArray = uniqueCount;
-      // var count = 0;
-
-      // if (consumerDetails[0].recordPurchased.length > 0) {
-      //   for (let countryRecord of consumerDetails[0].recordPurchased) {
-      //     count += countryRecord.records.length;
-      //   }
-      // }
-      // consumerDetails[0].recordPurchased = count;
-      return consumerDetails;
-    } else {
-      res.status(404).json({
-        data: {
-          type: "MISSING",
-          msg: "Details Unavailable",
-          desc: "Details Not Found",
-        },
-      });
-    }
-  } catch (error) {
-    logger.error(` DASHBOARD CONTROLLER================== ${JSON.stringify(error)}`);
-    res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
-}
-
-async function fetchConsumersDashboardByUser(accountId, userId, res) {
-  try {
-    const consumerDetails = await DashboardModel.findConsumerByUser(userId);
-    if (consumerDetails) {
-      const lower = consumerDetails[0].countryArray.map(element => {
-        return element.toLowerCase();
-      });
-      const uniqueCount = new Set(lower).size;
-      consumerDetails[0].countryArray = uniqueCount;
-      return consumerDetails;
-    } else {
-      res.status(404).json({
-        data: {
-          type: "MISSING",
-          msg: "Details Unavailable",
-          desc: "Details Not Found",
-        },
-      });
-    }
-  } catch (error) {
-    logger.error(` DASHBOARD CONTROLLER================== ${JSON.stringify(error)}`);
-    res.status(500).json({
-      message: "Internal Server Error",
-    });
-  }
 }
 
 module.exports = {
