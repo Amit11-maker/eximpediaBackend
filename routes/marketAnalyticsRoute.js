c82ec8ec8f1737a85d55e0a36418afa2cf93d487
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2023-01-31 14:36:08 +0530

    correcting format

--- routes/marketAnalyticsRoute.js
+++ routes/marketAnalyticsRoute.js
@@ -11,24 +11,24 @@ const AuthMiddleware = require('../middlewares/authMiddleware');
 
 
 // Route to analyse market data of companies as per two countries
-router.post('/companies/search', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchContryWiseMarketAnalyticsData);
-router.post('/companies/filters', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchContryWiseMarketAnalyticsFilters);
-router.post('/companies/download', AuthMiddleware.authorizeAccess , marketAnalyticsController.downloadContryWiseMarketAnalyticsData);
+router.post('/companies/search', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchContryWiseMarketAnalyticsData);
+router.post('/companies/filters', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchContryWiseMarketAnalyticsFilters);
+router.post('/companies/download', AuthMiddleware.authorizeAccess, marketAnalyticsController.downloadContryWiseMarketAnalyticsData);
 
 
 // Route to analyse country vs country market data as per the company
-router.post('/countries/search', AuthMiddleware.authorizeAccess , marketAnalyticsController.downloadContryWiseCompanyAnalyticsData);
-router.post('/countries/download', AuthMiddleware.authorizeAccess , marketAnalyticsController.downloadContryWiseCompanyAnalyticsData);
+router.post('/countries/search', AuthMiddleware.authorizeAccess, marketAnalyticsController.downloadContryWiseCompanyAnalyticsData);
+router.post('/countries/download', AuthMiddleware.authorizeAccess, marketAnalyticsController.downloadContryWiseCompanyAnalyticsData);
 
 
 // Route to analyse country vs product market data
-router.post('/product/search', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchProductWiseMarketAnalyticsData);
-router.post('/product/filter', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchProductWiseMarketAnalyticsFilters);
-router.post('/product/download', AuthMiddleware.authorizeAccess , marketAnalyticsController.downloadProductWiseMarketAnalyticsData);
+router.post('/product/search', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchProductWiseMarketAnalyticsData);
+router.post('/product/filter', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchProductWiseMarketAnalyticsFilters);
+router.post('/product/download', AuthMiddleware.authorizeAccess, marketAnalyticsController.downloadProductWiseMarketAnalyticsData);
 
 // Route to analyse country vs importer/exporter market data
-router.post('/trade/search', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchTradeWiseMarketAnalyticsData);
-router.post('/trade/filter', AuthMiddleware.authorizeAccess , marketAnalyticsController.fetchTradeWiseMarketAnalyticsFilters);
-router.post('/trade/download', AuthMiddleware.authorizeAccess , marketAnalyticsController.downloadTradeWiseMarketAnalyticsData);
+router.post('/trade/search', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchTradeWiseMarketAnalyticsData);
+router.post('/trade/filter', AuthMiddleware.authorizeAccess, marketAnalyticsController.fetchTradeWiseMarketAnalyticsFilters);
+router.post('/trade/download', AuthMiddleware.authorizeAccess, marketAnalyticsController.downloadTradeWiseMarketAnalyticsData);
 
 module.exports = router;
