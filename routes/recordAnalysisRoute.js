b65003a65c2640bc51224d1f453071a902cc8947
Kaushal Kanodia <kaushal.kanodia@mandelbulbtech.com>
2023-05-23 11:57:09 +0530

    Adding router to analysis route

--- routes/recordAnalysisRoute.js
+++ routes/recordAnalysisRoute.js
@@ -1,11 +1,17 @@
 const RecordController = require('../controllers/recordAnalysisController');
 
-// Log Time
+const AuthMiddleware = require("../middlewares/authMiddleware");
+const express = require("express");
+const router = express.Router({
+    mergeParams: true,
+});
+
 router.use(function timeLog(req, res, next) {
     //console.log('Time: ', Date.now());
     next();
 });
 
 //Command Segregation
-
 router.post('/shipments/explore/records', AuthMiddleware.authorizeAccess, RecordController.getRecordAnalysis);
+
+module.exports = router;
