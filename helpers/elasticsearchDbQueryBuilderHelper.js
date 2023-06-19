ebf453539bd6aabd5b2d1ecc9370a0a6bc4a6e83
kaushal.kanodia <kaushal.kanodia@mandelbulbtech.com>
2023-06-19 12:16:13 +0530

    analyser fix

--- helpers/elasticsearchDbQueryBuilderHelper.js
+++ helpers/elasticsearchDbQueryBuilderHelper.js
@@ -210,12 +210,11 @@ const addAnalyzer = async (payload, dataBucket) => {
   try {
     for (let matchExpression of payload.aggregationParams.matchExpressions) {
       if (matchExpression.expressionType == 203) {
-        for (let value of matchExpression.fieldValue) {
-          if (value.slice(-1).toLowerCase() == "y") {
-            // dataBucket = "india_import_all"
-            if(dataBucket.substring(dataBucket.length - 1) == "*"){
-              dataBucket = dataBucket.substring(0, dataBucket.length -1)
-            }
+        for (let value of matchExpression.fieldValue) { 
+          if (value.slice(-1).toLowerCase() == "y") { 
+            if(dataBucket.substring(dataBucket.length - 1) == "*"){ 
+              dataBucket = dataBucket.substring(0, dataBucket.length -1) 
+            } 
             let analyzerOutput =
                 await ElasticsearchDbHandler.dbClient.indices.analyze({
                 index: dataBucket,
