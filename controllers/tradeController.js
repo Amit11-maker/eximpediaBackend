162cdb491864d65dc868ee3f3210d5b8b7b2165e
Jaya Maheshwari <jaya.maheshwari@admineximpedia.onmicrosoft.com>
2023-09-26 08:20:37 +0000

    fixed count route

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -1188,6 +1188,15 @@ const fetchAdxSuggestions = async (req, res) => {
     res.status(500).json({ message: "Internal server error!" })
   }
 }
+const fetchcount = async (req, res) => {
+  try{
+        const results = await TradeModel.getRecordscount(req.body);
+        res.status(200).json(results)
+  }
+ catch (err){
+        res.status(500).json({ message: "Internal server error!" })
+}
+}
 
 module.exports = {
   fetchExploreCountries,
@@ -1208,5 +1217,6 @@ module.exports = {
   fetchAdxData,
   fetchAdxFilters,
   fetchAdxSuggestions,
-  fetchAdxRecordsSummary
+  fetchAdxRecordsSummary,
+  fetchcount
 };
