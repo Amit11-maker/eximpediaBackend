319aeeb54269ac5f7238d52f414ced4ab14da957
Jaya Maheshwari <jaya.maheshwari@admineximpedia.onmicrosoft.com>
2023-09-26 08:42:48 +0000

    Updated tradeController.js

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -1188,15 +1188,6 @@ const fetchAdxSuggestions = async (req, res) => {
     res.status(500).json({ message: "Internal server error!" })
   }
 }
-const fetchcount = async (req, res) => {
-  try{
-        const results = await TradeModel.getRecordscount(req.body);
-        res.status(200).json(results)
-  }
- catch (err){
-        res.status(500).json({ message: "Internal server error!" })
-}
-}
 
 module.exports = {
   fetchExploreCountries,
@@ -1218,5 +1209,4 @@ module.exports = {
   fetchAdxFilters,
   fetchAdxSuggestions,
   fetchAdxRecordsSummary,
-  fetchcount
 };
