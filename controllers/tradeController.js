8981bf1605e9eddbed63a94708e478da138f9ecc
kaushal.kanodia <kaushal.kanodia.01@gmail.com>
2023-10-26 14:25:46 +0530

    Commenting view summary

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -882,17 +882,17 @@ const fetchCompanyDetails = async (req, res, isrecommendationDataRequest) => {
       }
     }
 
-    const tradeCompanies = await TradeModel.findCompanyDetailsByPatternEngineADX(
-      { country, tradeType },
-      searchTerm,
-      tradeMeta,
-      startDate,
-      endDate,
-      searchingColumns,
-      isrecommendationDataRequest
-    );
+    // const tradeCompanies = await TradeModel.findCompanyDetailsByPatternEngineADX(
+    //   { country, tradeType },
+    //   searchTerm,
+    //   tradeMeta,
+    //   startDate,
+    //   endDate,
+    //   searchingColumns,
+    //   isrecommendationDataRequest
+    // );
 
-    res.send(tradeCompanies);
+    // res.send(tradeCompanies);
 
     if (isrecommendationDataRequest) {
       return tradeCompanies.FILTER_BUYER_SELLER;
